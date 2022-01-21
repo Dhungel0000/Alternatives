bcf5245d62707d393838b180a660b8909ce313af
ADHUNGEL <Aashrika.Dhungel@otto.de>
2022-01-21 09:20:58 +0100

    Initial

---
+++ src/main/java/de/otto/alternatives/AlternativesApplication.java
@@ -0,0 +1,13 @@
+package de.otto.alternatives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternativesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlternativesApplication.class, args);
+  }
+
+}
